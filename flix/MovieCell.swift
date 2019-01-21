b38d19cab45e0dacd1e795860cc61740926f73d1
Rohith Sridharan <rohith@Rohiths-MacBook-Pro.local>
2019-01-21 18:10:15 -0500

    added files

---
+++ flix/MovieCell.swift
@@ -0,0 +1,29 @@
+ //
+//  MovieCell.swift
+//  flix
+//
+//  Created by Rohith Sridharan on 1/17/19.
+//  Copyright © 2019 Rohith Sridharan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var imageLabel: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
